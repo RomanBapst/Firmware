a80ba7014ca0fb8104b2be7049485fb3de80c8bf
tumbili <bapstr@ethz.ch>
2015-05-27 08:48:22 +0200

    hardcode vehicle type to rotary wing so that attitude and rates are controller by default

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -2389,6 +2389,7 @@ set_control_mode()
 	control_mode.flag_external_manual_override_ok = (!status.is_rotary_wing && !status.is_vtol);
 	control_mode.flag_system_hil_enabled = status.hil_state == vehicle_status_s::HIL_STATE_ON;
 	control_mode.flag_control_offboard_enabled = false;
+	status.is_rotary_wing = true;
 
 	switch (status.nav_state) {
 	case vehicle_status_s::NAVIGATION_STATE_MANUAL:
