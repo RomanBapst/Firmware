6b4a773e599a0f110dda6d5fe770819d88d804a8
Roman <bapstroman@gmail.com>
2018-02-27 10:19:53 +0100

    cmake configs: we don't yet have the imu directory
    Signed-off-by: Roman <bapstroman@gmail.com>

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -33,7 +33,7 @@ set(config_module_list
 	drivers/lsm303d
 	#drivers/mb12xx
 	#drivers/mkblctrl
-	drivers/imu/mpu6000
+	drivers/mpu6000
 	#drivers/imu/mpu9250
 	#drivers/oreoled
 	#drivers/protocol_splitter
